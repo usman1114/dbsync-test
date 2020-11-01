ecde9788d960c14d2403a0ac9de47ab0221a1d63
usman.zubair <usman.zubair@databricks.com>
2020-11-01 11:17:17 -0500

    Deleted via databricks-sync.

--- variables_env.sh
+++
@@ -1,7 +0,0 @@
-export TF_VAR_IntegrationTest_scope1_it_key1_var=
-export TF_VAR_IntegrationTest_scope2_it_key2_var=
-export TF_VAR_uz_IntegrationTest_scope1_it_key1_var=
-export TF_VAR_uz_IntegrationTest_scope2_it_key2_var=
-export TF_VAR_uz_IntegrationTest_scope1_uz_key1_var=
-export TF_VAR_uz_IntegrationTest_scope2_uz_key2_var=
-export TF_VAR_uz_IntegrationTest_scope3_db_password_var=
